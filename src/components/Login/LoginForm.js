5e008478abe2b3655864d61bbb92db6b7d53667d
dansmog <juwonanthony@gmail.com>
2017-09-14 08:33:13 +0100

    Add statusbar

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, StatusBar } from 'react-native'
 
 export default class LoginForm extends PureComponent {
 
@@ -9,24 +9,39 @@ export default class LoginForm extends PureComponent {
     render(){
         return (
             <View style={ styles.container }>
+                <StatusBar
+                    barStyle="light-content"
+                />
                 <TextInput 
                     style={ styles.input }
                     placeholder="username or email"
-                    placeholderTextColor="rgba(255,255,255,0.7)"
+                    placeholderTextColor="#3498db"
                     returnKeyType="next"
+                    onSubmitEditing={ () => this.passwordInput.focus() }
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
                 />
                 <TextInput 
                     style={ styles.input }
                     placeholder="password"
                     secureTextEntry
-                    placeholderTextColor="rgba(255,255,255,0.7)"  
-                    returnKeyType="go"                  
+                    placeholderTextColor="#3498db"  
+                    returnKeyType="go"
+                    ref={ input => this.passwordInput = input }                  
                 />
                 <TouchableOpacity 
                     style={ styles.buttonContainer }
                     onPress={ this._handleLogin } 
                 >
                     <Text style={ styles.buttonText }>Login</Text>
+                </TouchableOpacity>
+                <Text style={{ textAlign: 'center', color: '#fff', marginTop: 10 }}>OR</Text>
+                <TouchableOpacity 
+                    style={ styles.githubButton }
+                    onPress={ this._handleLogin } 
+                >
+                    <Text style={ styles.buttonText }>Login with Github</Text>
                 </TouchableOpacity>
             </View>
         )
@@ -39,9 +54,9 @@ const styles = StyleSheet.create({
     },
     input: {
         height: 40,
-        backgroundColor: 'rgba(255,255,255, 0.2)',
+        backgroundColor: 'rgba(255,255,255, 1)',
         marginBottom:10,
-        color: '#fff',
+        color: '#3498db',
         paddingHorizontal: 10
     },
     buttonContainer: {
@@ -52,5 +67,10 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: '#fff',
         fontWeight: '700'
+    },
+    githubButton: {
+        backgroundColor: '#27ae60',
+        paddingVertical: 15,
+        marginTop: 10
     }
 })
